c4ae24906346376b344828b20b1831c08f40ab17
s1ght test <s1ght@email.com>
2026-05-06 21:58:18 +0100

    fix css import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,11 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "s1ghts profile",
-  description: "peripheral reviewer | multi fps aimer 愛",
+  description: "peripheral reviewer | multi fps aimer",
 };
 
 export default function RootLayout({
@@ -17,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${inter.className} h-full antialiased`}>
-      <body className="min-h-full bg-black text-white">
+    <html lang="en">
+      <body className={`${inter.className} min-h-screen bg-black text-white`}>
         {children}
       </body>
     </html>
