eef0f84e6f49646538774487dde6182dff7857d4
s1ghtcode <s1ght.crossspot@gmail.com>
2026-05-07 10:53:09 +0100

    Live Site deployment
    maybe updated logo now

--- next.config.js
+++ next.config.js
@@ -2,3 +2,12 @@
 const nextConfig = {};
 
 module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  devIndicators: {
+    buildActivity: false,
+  },
+};
+
+module.exports = nextConfig;
